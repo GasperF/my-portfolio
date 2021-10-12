6ea4a71805f2be47843cf482a9809fec2a75ce47
Gaspar Flores <gasparflores107@gmail.com>
2021-10-12 17:56:24 -0400

    Add files via upload
    First Commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// Nav Stuff
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navIconEl = document.querySelector('.nav__icon');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+  document.body.style.visibility = 'visible';
+});
+
+const navOpen = () => {
+   navList.classList.add('show');
+  navBgOverlayEl.classList.add('active');
+  document.body.style= 'visibility: visible; height: 100vh; width:100vw; overflow: hidden;';
+}
+
+const navClose = () => {
+  navList.classList.remove('show');
+  navBgOverlayEl.classList.remove('active');
+  document.body.style= 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+navIconEl.addEventListener('click', navOpen);
+
+navCloseEl.addEventListener('click', navClose);
+
+navBgOverlayEl.addEventListener('click', navClose)
+
+// Modal stuff
+
+const open = document.getElementById('open');
+const modalContainer = document.getElementById('modal__container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () =>{
+  modalContainer.classList.add('show')
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${window.scrollY}px`;
+})
+
+close.addEventListener('click', () =>{
+  modalContainer.classList.remove('show');
+  document.body.style.position = '';
+  document.body.style.top = '';
+});
+
+
+// AOS
+AOS.init({
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
